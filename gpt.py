46b370335667b1a215571f620b722863e3dc3448
pavithra <pavs@stanford.edu>
2024-06-24 08:56:28 -0700

    Update code for new Dropbox file structure + xslx error

--- gpt.py
+++ gpt.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from dotenv import find_dotenv
 load_dotenv(find_dotenv())
 import pandas as pd
-
+import xlsxwriter
 
 def query_gpt(system_prompt, paper_content, df, save_path):
     df['Answer'] = df['Answer'].astype(object)
@@ -16,8 +16,6 @@ def query_gpt(system_prompt, paper_content, df, save_path):
 
         # print(df.loc["question", i])
         # print(message)
-
-
 
         ## Set the API key and model name
         MODEL="gpt-4-turbo-2024-04-09"
@@ -32,12 +30,12 @@ def query_gpt(system_prompt, paper_content, df, save_path):
             temperature=0
         )
 
-        print("Assistant: " + completion.choices[0].message.content)
+        #print("Assistant: " + completion.choices[0].message.content)
 
         df.at[i, 'Answer'] = completion.choices[0].message.content.split("Reference:")[0]
         df.at[i, 'Reference Sentences'] = completion.choices[0].message.content.split("Reference:")[1]
 
-    df.to_excel(save_path, index=False)
+    df.to_excel(save_path, index=False,engine='xlsxwriter') #to prevent encode errors use xlsxwriter
     return df
 
 
@@ -57,14 +55,17 @@ excel_path = 'Questions_Jun_21.xlsx'
 df = pd.read_excel(excel_path)
 
 
-
-for filename in os.listdir("checked_papers"):
-    if filename.endswith('.md'):
-        # Construct full file path
-        paper_path = os.path.join("checked_papers", filename)
-        pmid = filename.split(".")[0]
-        pmid = pmid.split("(")[0]
-        save_path = f"output/{pmid}_answers_Jun4.xlsx"
-        with open(paper_path, 'r', encoding='utf-8') as file:
-            paper_content = file.read()
-            query_gpt(system_prompt, paper_content, df, save_path)
+PATH="/Users/pavs/Dropbox/ChatGPT/Project two/paper Pavs/"
+for folder in os.listdir(PATH):
+    for filename in os.listdir(PATH+folder):
+        if filename.endswith('.md'):
+            # Construct full file path
+            paper_path = os.path.join(PATH + folder + '/' + filename)
+            pmid = filename.split(".")[0]
+            pmid = pmid.split("(")[0]
+            os.mkdir(PATH+folder+'/output/')
+            save_path = PATH + folder + '/output/'+pmid+'_answers_Jun24.xlsx'
+            print(paper_path,save_path)
+            with open(paper_path, 'r', encoding='utf-8') as file:
+                paper_content = file.read()
+                query_gpt(system_prompt, paper_content, df, save_path)
